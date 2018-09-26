a63b5d3cb4dabccfbc4af78d63af47f03f2e1d8c
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-09-26 14:14:03 +0200

    last update

---
+++ src/test/java/com/scc/lofselectclub/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.scc.lofselectclub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.scc.confsvr.ConfigServerApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ConfigServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+}
