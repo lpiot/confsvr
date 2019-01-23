693ed2f818025fd90aeec930927bab82cc89f5a4
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2019-01-23 16:24:39 +0100

    Fix issue #1

--- src/test/java/com/scc/confsvr/ApplicationTest.java
+++ src/test/java/com/scc/confsvr/ApplicationTest.java
@@ -1,15 +1,15 @@
-package com.scc.lofselectclub;
+package com.scc.confsvr;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.scc.confsvr.ConfigServerApplication;
+import com.scc.confsvr.Application;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ConfigServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ConfigServerApplicationTest {
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
 
 	@Test
 	public void contextLoads() {
